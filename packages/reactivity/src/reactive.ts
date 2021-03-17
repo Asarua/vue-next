cf84c9b2c043dd549cb47ac0aa2efdaa306a0bd5
Asarua <1299458195@qq.com>
2021-03-17 13:40:27 +0800

    feat(reactive.ts、ref.ts): 添加reactive和ref两个文件的注释

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -12,13 +12,19 @@ import {
 } from './collectionHandlers'
 import { UnwrapRef, Ref } from './ref'
 
+// reactive 响应性标识符
 export const enum ReactiveFlags {
+  // 用来标记跳过代理
   SKIP = '__v_skip',
+  // 用来标记响应对象
   IS_REACTIVE = '__v_isReactive',
+  // 用来标记只读对象
   IS_READONLY = '__v_isReadonly',
+  // 用来标记被代理的原生对象
   RAW = '__v_raw'
 }
 
+// 被代理的对象，应该起码存在某一项
 export interface Target {
   [ReactiveFlags.SKIP]?: boolean
   [ReactiveFlags.IS_REACTIVE]?: boolean
@@ -26,37 +32,52 @@ export interface Target {
   [ReactiveFlags.RAW]?: any
 }
 
+// 响应对象字典
 export const reactiveMap = new WeakMap<Target, any>()
+// 只读对象字典
 export const readonlyMap = new WeakMap<Target, any>()
 
+// 代理的对象的类型
 const enum TargetType {
+  // 无效的
   INVALID = 0,
+  // 常规的
   COMMON = 1,
+  // 收集者
   COLLECTION = 2
 }
 
+// 根据数据类型获取类型标签
 function targetTypeMap(rawType: string) {
   switch (rawType) {
     case 'Object':
     case 'Array':
+      // 常规
       return TargetType.COMMON
     case 'Map':
     case 'Set':
     case 'WeakMap':
     case 'WeakSet':
+      // 收集者
       return TargetType.COLLECTION
     default:
+      // 无效
       return TargetType.INVALID
   }
 }
 
+// 获取要代理的对象的类别标签
 function getTargetType(value: Target) {
+  // 如果是属于被标记的跳过的，或者是被设置了不可以设置新的属性的对象
   return value[ReactiveFlags.SKIP] || !Object.isExtensible(value)
-    ? TargetType.INVALID
-    : targetTypeMap(toRawType(value))
+    ? // 它会被标记为无效的
+      TargetType.INVALID
+    : // 否则会添加类别标记
+      targetTypeMap(toRawType(value))
 }
 
 // only unwrap nested ref
+// 未包裹的嵌套ref对象
 type UnwrapNestedRefs<T> = T extends Ref ? T : UnwrapRef<T>
 
 /**
@@ -81,10 +102,13 @@ type UnwrapNestedRefs<T> = T extends Ref ? T : UnwrapRef<T>
  * count.value // -> 1
  * ```
  */
+// 重载reactive函数
 export function reactive<T extends object>(target: T): UnwrapNestedRefs<T>
 export function reactive(target: object) {
   // if trying to observe a readonly proxy, return the readonly version.
+  // 如果是被标记的只读对象
   if (target && (target as Target)[ReactiveFlags.IS_READONLY]) {
+    // 那么返回它本身
     return target
   }
   return createReactiveObject(
@@ -100,6 +124,7 @@ export function reactive(target: object) {
  * level properties are reactive. It also does not auto-unwrap refs (even at the
  * root level).
  */
+// 浅层响应对象，因为Proxy默认只代理一层，如果设置为浅层，那只会代理最外层
 export function shallowReactive<T extends object>(target: T): T {
   return createReactiveObject(
     target,
@@ -109,7 +134,9 @@ export function shallowReactive<T extends object>(target: T): T {
   )
 }
 
+// 基本数据类型
 type Primitive = string | number | boolean | bigint | symbol | undefined | null
+// 不需要进行深层只读的数据类型
 type Builtin = Primitive | Function | Date | Error | RegExp
 export type DeepReadonly<T> = T extends Builtin
   ? T
@@ -135,6 +162,7 @@ export type DeepReadonly<T> = T extends Builtin
  * Creates a readonly copy of the original object. Note the returned copy is not
  * made reactive, but `readonly` can be called on an already reactive object.
  */
+// 创建只读对象
 export function readonly<T extends object>(
   target: T
 ): DeepReadonly<UnwrapNestedRefs<T>> {
@@ -152,6 +180,7 @@ export function readonly<T extends object>(
  * returned properties.
  * This is used for creating the props proxy object for stateful components.
  */
+// 创建浅层只读对象
 export function shallowReadonly<T extends object>(
   target: T
 ): Readonly<{ [K in keyof T]: UnwrapNestedRefs<T[K]> }> {
@@ -163,67 +192,99 @@ export function shallowReadonly<T extends object>(
   )
 }
 
+// 创建响应式对象
 function createReactiveObject(
+  // 要代理对对象
   target: Target,
+  // 是否只读
   isReadonly: boolean,
+  // 基础handler
   baseHandlers: ProxyHandler<any>,
+  // 收集者handler
   collectionHandlers: ProxyHandler<any>
 ) {
+  // 如果不是对象
   if (!isObject(target)) {
+    // 在开发环境下输出警告
     if (__DEV__) {
       console.warn(`value cannot be made reactive: ${String(target)}`)
     }
+    // 直接返回
     return target
   }
   // target is already a Proxy, return it.
   // exception: calling readonly() on a reactive object
   if (
+    // 如果要代理的对象中，存在'__v_raw'这个属性，说明是已经代理过的
     target[ReactiveFlags.RAW] &&
+    // TODO
     !(isReadonly && target[ReactiveFlags.IS_REACTIVE])
   ) {
     return target
   }
   // target already has corresponding Proxy
+  // 根据是否只读，来获取响应的响应式对象收集者（声明在当前文件36行，添加在当前文件249行）
   const proxyMap = isReadonly ? readonlyMap : reactiveMap
+  // 判断是否存在代理对象
   const existingProxy = proxyMap.get(target)
+  // 如果存在，则使用已代理过的对象，不需要再次proxy代理，以此进行优化
   if (existingProxy) {
     return existingProxy
   }
   // only a whitelist of value types can be observed.
+  // 判断要代理的值的类型标签
   const targetType = getTargetType(target)
+  // 如果是无效的，则不进行代理，直接返回
   if (targetType === TargetType.INVALID) {
     return target
   }
+  // 创建代理
   const proxy = new Proxy(
     target,
+    // 判断是否是收集者类型，如果是则使用`collectionHandlers`，如果不是，使用默认的`baseHandlers`
     targetType === TargetType.COLLECTION ? collectionHandlers : baseHandlers
   )
+  // 在对应的响应式对象收集者中添加代理过的对象，用以下次使用直接调用，不需要再次代理
+  // （声明在当前文件36行，再次使用在当前文件229行）
   proxyMap.set(target, proxy)
+  // 返回代理过的对象
   return proxy
 }
 
+// 是否是响应式对象
 export function isReactive(value: unknown): boolean {
+  // 如果是只读对象
   if (isReadonly(value)) {
+    // 判断它代理的初始对象是否是响应的
     return isReactive((value as Target)[ReactiveFlags.RAW])
   }
+  // 是否存在并且带有"__v_isReactive"属性
   return !!(value && (value as Target)[ReactiveFlags.IS_REACTIVE])
 }
 
+// 是否是只读对象
 export function isReadonly(value: unknown): boolean {
+  // value存在，并且带有"__v_isReadonly"属性
   return !!(value && (value as Target)[ReactiveFlags.IS_READONLY])
 }
 
+// 是否代理过
 export function isProxy(value: unknown): boolean {
+  // 是否是响应式的或者只读的
   return isReactive(value) || isReadonly(value)
 }
 
+// 递归返回响应对象中的被代理的原生对象
 export function toRaw<T>(observed: T): T {
   return (
+    // 一层层递归，最后返回原生的observed
     (observed && toRaw((observed as Target)[ReactiveFlags.RAW])) || observed
   )
 }
 
+// 标记为不被代理的对象
 export function markRaw<T extends object>(value: T): T {
+  // 在value中添加"__v_skip"属性为true，在代理的过程中就会跳过（当前文件72行）
   def(value, ReactiveFlags.SKIP, true)
   return value
 }
--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -4,8 +4,10 @@ import { isArray, isObject, hasChanged } from '@vue/shared'
 import { reactive, isProxy, toRaw, isReactive } from './reactive'
 import { CollectionTypes } from './collectionHandlers'
 
+// 每个ref对象的标识
 declare const RefSymbol: unique symbol
 
+// 声明Ref类型，用于ref方法
 export interface Ref<T = any> {
   value: T
   /**
@@ -27,14 +29,18 @@ export type ToRefs<T = any> = {
   [K in keyof T]: T[K] extends Ref ? T[K] : Ref<UnwrapRef<T[K]>>
 }
 
+// 转换函数，用于ref对象创建的时候的值的转换
 const convert = <T extends unknown>(val: T): T =>
+  // 如果是对象，则使用reactive进行代理，否则返回传进来的值
   isObject(val) ? reactive(val) : val
 
+// 重载判断某个值是否是ref处理过的
 export function isRef<T>(r: Ref<T> | unknown): r is Ref<T>
 export function isRef(r: any): r is Ref {
   return Boolean(r && r.__v_isRef === true)
 }
 
+// 重载创建ref对象
 export function ref<T extends object>(value: T): ToRef<T>
 export function ref<T>(value: T): Ref<UnwrapRef<T>>
 export function ref<T = any>(): Ref<T | undefined>
@@ -42,6 +48,7 @@ export function ref(value?: unknown) {
   return createRef(value)
 }
 
+// 浅层创建ref对象
 export function shallowRef<T extends object>(
   value: T
 ): T extends Ref ? T : Ref<T>
@@ -51,63 +58,93 @@ export function shallowRef(value?: unknown) {
   return createRef(value, true)
 }
 
+// ref实体
 class RefImpl<T> {
+  // 值
   private _value: T
 
+  // 标识符
   public readonly __v_isRef = true
 
+  // 两个参数：值、是否浅层
   constructor(private _rawValue: T, public readonly _shallow = false) {
+    // 如果是浅层，则值直接就是传进来的值，如果不是，将其转换
     this._value = _shallow ? _rawValue : convert(_rawValue)
   }
 
+  // 读取值的时候
   get value() {
+    // 添加追踪
     track(toRaw(this), TrackOpTypes.GET, 'value')
+    // 返回当前值
     return this._value
   }
 
+  // 设置值的时候
   set value(newVal) {
+    // 如果新值和原本的值发生了改变，才进行后续的逻辑（优化）
     if (hasChanged(toRaw(newVal), this._rawValue)) {
+      // 原生值留存
       this._rawValue = newVal
+      // 对值进行转换
       this._value = this._shallow ? newVal : convert(newVal)
+      // 触发添加过的依赖项
       trigger(toRaw(this), TriggerOpTypes.SET, 'value', newVal)
     }
   }
 }
 
+// 创建ref引用对象
 function createRef(rawValue: unknown, shallow = false) {
+  // 如果是已经包裹过的
   if (isRef(rawValue)) {
+    // 直接返回
     return rawValue
   }
+  // 否则创建新的ref对象
   return new RefImpl(rawValue, shallow)
 }
 
+// 触发ref对象绑定的依赖事件
 export function triggerRef(ref: Ref) {
   trigger(toRaw(ref), TriggerOpTypes.SET, 'value', __DEV__ ? ref.value : void 0)
 }
 
+// 解ref，返回里面的值
 export function unref<T>(ref: T): T extends Ref<infer V> ? V : T {
   return isRef(ref) ? (ref.value as any) : ref
 }
 
+// 浅层未包裹的handler
 const shallowUnwrapHandlers: ProxyHandler<any> = {
+  // 获取未包裹的值
   get: (target, key, receiver) => unref(Reflect.get(target, key, receiver)),
   set: (target, key, value, receiver) => {
+    // 取得旧的值
     const oldValue = target[key]
+    // 如果旧的值是包裹的并且新的值是未包裹的
     if (isRef(oldValue) && !isRef(value)) {
+      // 那么只替换其中的value
       oldValue.value = value
       return true
+      // 否则
     } else {
+      // 直接修改值
       return Reflect.set(target, key, value, receiver)
     }
   }
 }
 
+// 代理Ref对象，用于运行时runtime
 export function proxyRefs<T extends object>(
   objectWithRefs: T
 ): ShallowUnwrapRef<T> {
+  // 如果是已经proxy代理过的
   return isReactive(objectWithRefs)
-    ? objectWithRefs
-    : new Proxy(objectWithRefs, shallowUnwrapHandlers)
+    ? // 直接返回
+      objectWithRefs
+    : // 返回代理对象
+      new Proxy(objectWithRefs, shallowUnwrapHandlers)
 }
 
 export type CustomRefFactory<T> = (
@@ -118,6 +155,39 @@ export type CustomRefFactory<T> = (
   set: (value: T) => void
 }
 
+// 自定义ref对象，可以自定义get以及set事件，通过暴露出来的tract和trigger来触发
+/*
+  官网示例
+
+  ```js
+    function useDebouncedRef(value, delay = 200) {
+      let timeout
+      return customRef((track, trigger) => {
+        return {
+          get() {
+            track()
+            return value
+          },
+          set(newValue) {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => {
+              value = newValue
+              trigger()
+            }, delay)
+          }
+        }
+      })
+    }
+
+    export default {
+      setup() {
+        return {
+          text: useDebouncedRef('hello')
+        }
+      }
+    }
+  ```
+*/
 class CustomRefImpl<T> {
   private readonly _get: ReturnType<CustomRefFactory<T>>['get']
   private readonly _set: ReturnType<CustomRefFactory<T>>['set']
@@ -133,30 +203,38 @@ class CustomRefImpl<T> {
     this._set = set
   }
 
+  // 在get的时候调用自定义的get事件
   get value() {
     return this._get()
   }
 
+  // 在set的时候调用自定义的set事件
   set value(newVal) {
     this._set(newVal)
   }
 }
 
+// 创建自定义ref引用
 export function customRef<T>(factory: CustomRefFactory<T>): Ref<T> {
   return new CustomRefImpl(factory) as any
 }
 
+// 将响应式对象中的每一项都添加ref引用
 export function toRefs<T extends object>(object: T): ToRefs<T> {
+  // 如果是在开发环境，并且传入的值不是响应式对象
   if (__DEV__ && !isProxy(object)) {
     console.warn(`toRefs() expects a reactive object but received a plain one.`)
   }
+  // 判断是数组还是对象
   const ret: any = isArray(object) ? new Array(object.length) : {}
+  // 将每一项都添加ref引用
   for (const key in object) {
     ret[key] = toRef(object, key)
   }
   return ret
 }
 
+// 更改对象中的某个值为响应式
 class ObjectRefImpl<T extends object, K extends keyof T> {
   public readonly __v_isRef = true
 
@@ -171,13 +249,17 @@ class ObjectRefImpl<T extends object, K extends keyof T> {
   }
 }
 
+// 将对象中的某个值更改为ref响应式
 export function toRef<T extends object, K extends keyof T>(
   object: T,
   key: K
 ): ToRef<T[K]> {
+  // 如果该值已经是ref响应
   return isRef(object[key])
-    ? object[key]
-    : (new ObjectRefImpl(object, key) as any)
+    ? // 直接返回
+      object[key]
+    : // 对其进行ref响应代理
+      (new ObjectRefImpl(object, key) as any)
 }
 
 // corner case when use narrows type
